3fe9e8cf925e8548643f4b172b819da3a79112e1
stejanet <35765752+stejanet@users.noreply.github.com>
2018-01-24 11:37:17 +0000

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,92 @@
+<html>
+<head>
+
+<style>
+    .conv-list{
+        width:100%;
+        height:100%;
+        text-align:center;
+    }
+
+
+    .conv-wrapper{
+        width:90vw;
+        height:30vw;
+        background-color:#f00;
+    }
+
+    .conv-cover{
+        background-position-x:center;   
+        background-position-y:center;
+        background-size:cover;   
+        background-repeat:no-repeat;
+        width:30vw;
+        padding-top:30vw;
+        display:inline-block;
+        position:relative;
+        left:0;
+        margin:0;
+        padding-bottom:0;
+        padding-left:0;
+        padding-right:0;
+    }
+
+    .conv-content{
+        width:60%;
+        background-color:#0f0;
+        display:inline-block;
+        padding:0;
+        vertical-align:top;
+        text-align:left;
+        overflow-y:hidden;
+        max-height:100%;
+    }
+</style>
+
+</head>
+<body>
+
+<h1>Convenzioni</h1>
+<div class='conv-list'>
+
+    <div class='conv-wrapper'>
+
+        <div class='conv-cover' style="background-image: url('https://www.remkes.com/wp-content/uploads/2016/04/printable-coupons.jpg')">
+        </div>
+        <div class='conv-content'>
+
+            <h2 class='conv-title'>Titolo convenzione</h2>
+            <div class='conv-description'>
+            
+            
+            </div>
+
+
+
+
+        </div>
+        
+
+
+    </div>
+
+
+
+
+
+</div>
+
+
+
+
+<?php
+
+
+
+
+
+?>
+
+
+</body>
+</html>
