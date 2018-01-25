cbaf3308a07d70e6194b9744df99e757353c5cc4
AliceZoemem <alicealbertin90@gmail.com>
2018-01-25 10:00:36 +0100

    Add files via upload

--- convenzione.php
+++ convenzione.php
@@ -15,10 +15,10 @@
 	<body>
 		<div class="container-fluid">
 			<div class="row">
-				<div class="col-sm-6">
+				<div class="col-sm-6" id="contenuto_img">
 					<img id="img_convenction" class="slider" src="/img/e.png"/>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-sm-6" id="contenuto_testo">
 					<h2 id="title_convenction" >Convenzione 1</h2>
 					<div >
 						<p id="descrizione">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
@@ -26,11 +26,11 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-4 col-md-auto" id="allegati">
+				<div class="col-4 col-md-auto" id="contenuto_allegati">
 					<ul>
-						<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</li>
-						<li>sf</li>
-						<li>agdg</li>
+						<li>Allegato 1</li>
+						<li>Allegato 2</li>
+						<li>Allegato 3</li>
 					</ul>
 				</div>
 			</div>
