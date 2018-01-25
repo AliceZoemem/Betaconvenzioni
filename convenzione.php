669c56d4f7d8a25ea6632fe304118a263cda2ce2
AliceZoemem <alicealbertin90@gmail.com>
2018-01-25 13:57:10 +0100

    Add files via upload

--- convenzione.php
+++ convenzione.php
@@ -1,3 +1,26 @@
+<?php
+	// session_start();
+	// $id_convenzione = $_GET['convenzione'];
+	$id_convenzione = 1;
+	$servername = "localhost";
+	$db_username = "root";
+	$db_pw = "";
+	$db_name = "db_betaconvenzioni";
+	$conn = new mysqli($servername, $db_username, $db_pw, $db_name);
+	$sql = "SELECT * FROM tbl_convenzioni WHERE IdConvenzione = " . $id_convenzione;
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		foreach ($result as $row) {
+			// print_r ($row['Titolo']);
+			echo "<script>
+				document.getElementById('descrizione').innerHTML = '" .$row['Descrizione'] ."';
+				document.getElementById('title_convenction').innerHTML = '" .$row['Titolo']."';
+			</script>";
+		}
+	}
+	
+?>	
 <!DOCTYPE HTML>  
 <html lang="it">
 <html>
@@ -5,38 +28,118 @@
 		<title>Betacom_Convenzione</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="/css/stile.css">
-		<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="/css/css-stars.css">
+		<script src="/js/jquery/jquery.min.js"></script>
+		<script src="/js/jquery/jquery-1.11.2.min.js"></script> 
+		<script src="/js/jquery/jquery.barrating.js"></script>
+		<script src="/js/examples.js"></script>	
+		<link rel="stylesheet" type="text/css" href="/css/stile.css">		
+		<link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
 		<script src="/js/bootstrap.js"></script>
+		<script>$('.carousel').carousel();</script>
+		<link rel="stylesheet" href="/css/bootstrap.min.css">		
+		<script src="/js/bootstrap.min.js"></script>
+		
+		
+		
+		<style>
+			<!--.carousel-item{
+				width:50%;
+				height:400px;
+				background-position-x:center;
+				background-position-y:center;
+				background-repeat:no-repeat;
+				background-size:cover;
+			}-->
+		</style>
+		
+		
+		
+		
 	</head>
 	<body>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-sm-6" id="contenuto_img">
-					<img id="img_convenction" class="slider" src="/img/e.png"/>
+					<!--<img id="img_convenction" class="slider" src="/img/e.png"/>-->
+					<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+						<ol class="carousel-indicators">
+							<?php
+								// $sql_immagini = "SELECT * FROM tbl_immagini WHERE IdConvenzione = " . $id_convenzione;
+								// $result_immagini = $conn->query($sql_immagini);
+
+								// foreach ($result_immagini->result() as $row)
+								// {
+									// if($row != 0)
+										// echo '<li data-target="#carouselExampleIndicators" data-slide-to="' .$row. '"></li>';	
+									// else
+										// echo '<li data-target="#carouselExampleIndicators" data-slide-to="' .$row. '" class="active"></li>'
+								// }
+								
+							?>
+							<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+							<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+								<img class="d-block w-100" src="/img/i1.jpg" alt="First slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="/img/i2.jpg" alt="Second slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="/img/i3.jpg" alt="Second slide">
+							</div>
+							<div class="carousel-item">
+								<img class="d-block w-100" src="/img/i4.jpg" alt="Second slide">
+							</div>
+							
+						</div>
+						<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+							<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+							<span class="carousel-control-next-icon" aria-hidden="true"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
 				</div>
 				<div class="col-sm-6" id="contenuto_testo">
 					<h2 id="title_convenction" >Convenzione 1</h2>
-					<div >
+					<div>
 						<p id="descrizione">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 					</div>
+					<div class="stars stars-example-css">
+						<select id="example-css" name="rating" autocomplete="off">
+							<option value="1">1</option>
+							<option value="2">2</option>
+							<option value="3">3</option>
+							<option value="4">4</option>
+							<option value="5">5</option>
+						</select> 
+					</div>  
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-4 col-md-auto" id="contenuto_allegati">
-					<ul>
-						<li>Allegato 1</li>
-						<li>Allegato 2</li>
-						<li>Allegato 3</li>
+					<ul id="elenco_allegati">
+						<?php
+							// $sql_allegati = "SELECT * FROM tbl_allegati WHERE IdConvenzione = " . $id_convenzione;
+							// $result_allegati = $conn->query($sql_allegati);
+
+							// foreach ($result_allegati->result() as $row)
+							// {
+								// echo '<li><a href="/allegati/'. $row->NomeFile .'">'. $row->NomeFile .'</a></li>';								
+							// }
+							
+						?>
+
+						
 					</ul>
 				</div>
 			</div>
 		</div>	
 	</body>
 </html>
-<?php
-	
-?>	
--- db_betaconvenzioni.sql
+++ db_betaconvenzioni.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 4.4.14
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Creato il: Gen 24, 2018 alle 11:35
--- Versione del server: 10.1.29-MariaDB
--- Versione PHP: 7.1.12
+-- Creato il: Gen 25, 2018 alle 13:56
+-- Versione del server: 5.6.26
+-- Versione PHP: 5.6.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,11 +26,22 @@ SET time_zone = "+00:00";
 -- Struttura della tabella `tbl_allegati`
 --
 
-CREATE TABLE `tbl_allegati` (
+CREATE TABLE IF NOT EXISTS `tbl_allegati` (
   `IdAllegato` int(11) NOT NULL,
   `NomeFile` varchar(250) NOT NULL,
   `IdConvenzione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `tbl_allegati`
+--
+
+INSERT INTO `tbl_allegati` (`IdAllegato`, `NomeFile`, `IdConvenzione`) VALUES
+(1, '@900 - Copia.pdf', 1),
+(2, '1984-it.pdf', 1),
+(3, 'animalfarmit.pdf', 1),
+(4, 'l-agnese-va-a-morire-8806174843.pdf', 1),
+(5, 'BraveNewWorld.pdf', 2);
 
 -- --------------------------------------------------------
 
@@ -40,10 +49,18 @@ CREATE TABLE `tbl_allegati` (
 -- Struttura della tabella `tbl_categorie`
 --
 
-CREATE TABLE `tbl_categorie` (
+CREATE TABLE IF NOT EXISTS `tbl_categorie` (
   `IdCategoria` int(11) NOT NULL,
   `Nome` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `tbl_categorie`
+--
+
+INSERT INTO `tbl_categorie` (`IdCategoria`, `Nome`) VALUES
+(1, 'automobili'),
+(2, 'sport');
 
 -- --------------------------------------------------------
 
@@ -51,7 +68,7 @@ CREATE TABLE `tbl_categorie` (
 -- Struttura della tabella `tbl_convenzioni`
 --
 
-CREATE TABLE `tbl_convenzioni` (
+CREATE TABLE IF NOT EXISTS `tbl_convenzioni` (
   `IdConvenzione` int(11) NOT NULL,
   `Titolo` varchar(250) NOT NULL,
   `Descrizione` varchar(250) NOT NULL,
@@ -59,7 +76,16 @@ CREATE TABLE `tbl_convenzioni` (
   `DataCreazione` date NOT NULL,
   `DataScadenza` date NOT NULL,
   `IdCategoria` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `tbl_convenzioni`
+--
+
+INSERT INTO `tbl_convenzioni` (`IdConvenzione`, `Titolo`, `Descrizione`, `Posizione`, `DataCreazione`, `DataScadenza`, `IdCategoria`) VALUES
+(1, 'conv1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure do', '45.067507|7.583635', '2018-01-25', '2018-04-26', 1),
+(2, 'Convenzione2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure do', '45.067507|7.583635', '2018-01-09', '2018-10-05', 1),
+(3, 'conv3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure do', '45.070242|7.585244', '2018-01-01', '2018-01-22', 2);
 
 -- --------------------------------------------------------
 
@@ -67,7 +93,7 @@ CREATE TABLE `tbl_convenzioni` (
 -- Struttura della tabella `tbl_feedback`
 --
 
-CREATE TABLE `tbl_feedback` (
+CREATE TABLE IF NOT EXISTS `tbl_feedback` (
   `IdUtente` int(11) NOT NULL,
   `IdConvenzione` int(11) NOT NULL,
   `Voto` int(11) NOT NULL
@@ -79,12 +105,24 @@ CREATE TABLE `tbl_feedback` (
 -- Struttura della tabella `tbl_immagini`
 --
 
-CREATE TABLE `tbl_immagini` (
+CREATE TABLE IF NOT EXISTS `tbl_immagini` (
   `IdImmagine` int(11) NOT NULL,
   `NomeFile` varchar(250) NOT NULL,
   `Ordine` int(11) NOT NULL,
   `IdConvenzione` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+--
+-- Dump dei dati per la tabella `tbl_immagini`
+--
+
+INSERT INTO `tbl_immagini` (`IdImmagine`, `NomeFile`, `Ordine`, `IdConvenzione`) VALUES
+(1, 'i1.jpg', 1, 1),
+(2, 'i2.jpg', 2, 1),
+(3, 'i3.jpg', 3, 1),
+(4, 'i4.jpg', 4, 1),
+(5, 'i5.jpg', 1, 2),
+(6, 'i6.jpg', 2, 2);
 
 -- --------------------------------------------------------
 
@@ -92,7 +130,7 @@ CREATE TABLE `tbl_immagini` (
 -- Struttura della tabella `tbl_log`
 --
 
-CREATE TABLE `tbl_log` (
+CREATE TABLE IF NOT EXISTS `tbl_log` (
   `IdUtente` int(11) NOT NULL,
   `IdConvenzione` int(11) NOT NULL,
   `Contatore` int(11) NOT NULL
@@ -104,7 +142,7 @@ CREATE TABLE `tbl_log` (
 -- Struttura della tabella `tbl_utenti`
 --
 
-CREATE TABLE `tbl_utenti` (
+CREATE TABLE IF NOT EXISTS `tbl_utenti` (
   `IdUtente` int(11) NOT NULL,
   `Cognome` varchar(250) NOT NULL,
   `Nome` varchar(250) NOT NULL,
@@ -113,7 +151,7 @@ CREATE TABLE `tbl_utenti` (
   `Posizione` varchar(250) NOT NULL,
   `IsAmminstratore` tinyint(1) NOT NULL,
   `Attivo` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `tbl_utenti`
@@ -177,33 +215,27 @@ ALTER TABLE `tbl_utenti`
 -- AUTO_INCREMENT per la tabella `tbl_allegati`
 --
 ALTER TABLE `tbl_allegati`
-  MODIFY `IdAllegato` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `IdAllegato` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT per la tabella `tbl_categorie`
 --
 ALTER TABLE `tbl_categorie`
-  MODIFY `IdCategoria` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `IdCategoria` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT per la tabella `tbl_convenzioni`
 --
 ALTER TABLE `tbl_convenzioni`
-  MODIFY `IdConvenzione` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `IdConvenzione` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT per la tabella `tbl_immagini`
 --
 ALTER TABLE `tbl_immagini`
-  MODIFY `IdImmagine` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `IdImmagine` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT per la tabella `tbl_utenti`
 --
 ALTER TABLE `tbl_utenti`
-  MODIFY `IdUtente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
+  MODIFY `IdUtente` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
