e0801b9048021fe19a079696aa3d86894c292ccf
stejanet <35765752+stejanet@users.noreply.github.com>
2018-02-02 13:18:01 +0100

    Add files via upload

--- functions/functions.php
+++ functions/functions.php
@@ -6,13 +6,34 @@
 </body>
 </html>
 
-
 <?php
 
 if(isset($_GET['function'])) {
     $function = $_GET['function'];
     $function();
 }
+
+function Encryption($string, $action = 'e' ) { //parametri: string (stringa da criptare/decriptare), action (se action = 'e' --> encrypt (cripta), se action = 'd' --> decryption (decripta))
+    // you may change these values to your own
+    $secret_key = 'my_simple_secret_key';
+    $secret_iv = 'my_simple_secret_iv';
+ 
+    $output = false;
+    $encrypt_method = "AES-256-CBC";
+    $key = hash( 'sha256', $secret_key );
+    $iv = substr( hash( 'sha256', $secret_iv ), 0, 16 );
+ 
+    if( $action == 'e' ) {
+        $output = base64_encode( openssl_encrypt( $string, $encrypt_method, $key, 0, $iv ) );
+    }
+    else if( $action == 'd' ){
+        $output = openssl_decrypt( base64_decode( $string ), $encrypt_method, $key, 0, $iv );
+    }
+ 
+    return $output;
+}
+
+
 
 function LoadList() { //Parametri: categoria (?), cerca (?)
     $link = mysqli_connect("localhost", "root", "", "db_betaconvenzioni");
@@ -46,7 +67,8 @@ function LoadList() { //Parametri: categoria (?), cerca (?)
             $idConvenzione = $row["IdConvenzione"];
             $titolo = $row['Titolo'];
             $descrizione = $row['Descrizione'];
-            $coordinate = $row['Posizione'];
+            $lat = $row['Lat'];
+			$lng = $row['Lng'];
             $scadenza = $row['DataScadenza'];
             $idCategoria = $row['IdCategoria'];
             
